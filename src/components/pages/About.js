6d1fccd4912067c2ee34e64a12c402ee45568400
Clyde Clark <clydeclark78@gmail.com>
2020-12-02 09:28:43 -0800

    Finished About.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import clydeImage from "../../Resources/Clyde.jpg";
+import clydeResume from "../../Resources/Clyde_Clark_Resume.pdf"
+
+function About() {
+    return(
+        <div>
+            <div class="col-md-12">
+                <h1>About Me</h1>
+            </div>
+            <div class="container">
+                <div class="image-wrapper float-left pr-3">
+                    <img width="300px" src={clydeImage} alt="Clyde"/>
+                </div>
+                <div class="single-post-content-wrapper p-3">
+                        <p>I'm a US Navy veteran and current full-stack developer with experience in the MERN stack (MongoDB, Express, React, Node.js). I
+                        especially enjoy the coding side of web development.</p>
+                        <p>I really love learning and consider it essential for my own happiness. In my spare time, I like
+                        learning more about programming, tabletop role playing, and personal development.</p>
+                        <p>Find me on <a href="https://www.linkedin.com/in/clyde-clark-8919541b0/" rel="noopener noreferrer">LinkedIn</a></p>
+                        <p>Access my <a href={clydeResume}>Resume</a></p>
+                </div>
+            </div>
+
+        </div>
+    )
+};
+
+export default About;
